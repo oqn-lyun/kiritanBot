07f369b5b32fbcc65182dee69b29c118a5057164
OQN <luta00000@gmail.com>
2021-11-13 01:38:41 +0900

    ディレクトリ名変更

--- src/weather-open-meteo/open-meteo.ts
+++ src/weather-open-meteo/open-meteo.ts
@@ -4,8 +4,8 @@ import { OpenMeteoObject, isOpenMeteoObject } from "./open-meteo-types";
 export const getOpenMeteoObject = async (): Promise<OpenMeteoObject> => {
   const url = "https://api.open-meteo.com/v1/forecast";
   const query = {
-    latitude: 34.4111,
-    longitude: 135.3112,
+    latitude: 34.8,
+    longitude: 135.46,
     hourly: "temperature_2m",
     daily: "weathercode,temperature_2m_max,temperature_2m_min",
     timezone: "Asia/Tokyo",
@@ -13,6 +13,7 @@ export const getOpenMeteoObject = async (): Promise<OpenMeteoObject> => {
 
   const res = await got(stringifyUrl({ url, query }, { encode: false }));
   const resbody = JSON.parse(res.body) as unknown;
+  console.log(resbody);
   if (!isOpenMeteoObject(resbody)) {
     throw new Error("invalid response");
   }
